e0846431c32ed9b43bc7b0894f9e74865cef35ec
paul <paul@securly.com>
2016-06-01 14:45:06 -0700

    better gif search terms

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -15,26 +15,48 @@ angular.module('myApp.view1', ['ngRoute'])
     $scope.timer = 2000;
     var tag = '';
     var timerRule = true;
+    var api = 0;
+    var urls = [
+        'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='/*,*/
+        /*'http://replygif.net/api/gifs?tag=',*/
+        /*'https://api.popkey.co/v2/media/random?q='*/
+    ];
     $scope.getImage = function() {
+        
         $http({
           method: 'GET',
-          url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC' + tag
+          url: urls[api] + tag
         }).then(function successCallback(response) {
-                if (parseInt(response.data.data.image_width) < 160) {
+                
+                if (response.data && response.data.data && api == 0 && parseInt(response.data.data.image_width) < 160) {
                     $scope.getImage();
                     return;
                 }
-                $scope.image = response.data.data.image_original_url;
-                length = parseInt(response.data.data.image_frames);
-                $scope.timer = length *190;
+                if(response.length < 1) {
+                    $scope.getImage();
+                    return;
+                }
+                console.log(api)
+                if (api == 0) {
+                    length = parseInt(response.data.data.image_frames);
+                    $scope.image = response.data.data.image_original_url;
+                } else if (api == 1){   
+                    $scope.image = response[Math.floor((Math.random() * response.length))].file;
+                    length = 250;
+                }
+                if(length > 300)
+                    $scope.timer = length *95;
+                else 
+                    $scope.timer = length *190;
                 if ($scope.timer < 800)
                     $scope.timer = 2200;
                 if ($scope.timer > 4200 && timerRule == true)
                     $scope.timer = 4200;
                 $timeout(function(){
-                    setTag();
+                    setTag(api);
                     $scope.getImage();
-                }, $scope.timer);                            
+                }, $scope.timer);         
+                api = Math.floor((Math.random() * urls.length));
                 
           }, function errorCallback(response) {
             // called asynchronously if an error occurs
@@ -43,47 +65,50 @@ angular.module('myApp.view1', ['ngRoute'])
         
     };
     $scope.getImage();
-    function setTag() {
+    function setTag(id) {
         var arr = [];
-        var int = Math.floor((Math.random() * 5));
+        var apiKey = '';
+        if(id == 1)
+            apiKey = '&api-key=39YAprx5Yi';
+        var int = Math.floor((Math.random() * 7));
         if(aud.currentTime < 160){
             /*It's OK Now*/
-            arr= ['dancing+girl','ok','girl+power','africa','dance+crew', 'break+dance']
-            return tag = '&tag=' + arr[int];
+            arr= ['dancing+girl','ok','girl+power','africa','dance+crew', 'break+dance', 'girls']
+            return $scope.tag = tag = arr[int]+apiKey;
         } else if(aud.currentTime > 160 && aud.currentTime < 240){
             /*I'm a Soldier*/
-            arr= ['soldier','war','fight','struggle','guns', 'ninja']
-            return tag = '&tag=' + arr[int];
+            arr= ['soldier','war','fight','struggle','guns', 'ninja', 'explosion', 'battle']
+            return $scope.tag = tag = arr[int]+apiKey;
         } else if(aud.currentTime > 240 && aud.currentTime < 585){
             timerRule = false;
-            arr= ['nasa','technology','science','space','mind+blown', 'wow']
-            return tag = '&tag=' + arr[int];
+            arr= ['nasa','technology','science','space','mind+blown', 'wow', '']
+            return tag = arr[int]+apiKey;
         } else if(aud.currentTime > 585 && aud.currentTime < 730){
             timerRule = true;
-            arr= ['bruce+lee','lucy','sushi','anime','japan', 'tokyo']
-            return tag = '&tag=' + arr[int];
+            arr= ['bruce+lee','lucy','sushi','anime','japan', 'tokyo', 'asian']
+            return tag = arr[int]+apiKey;
         } else if(aud.currentTime > 730 && aud.currentTime < 895){
             timerRule = false;
-            arr= ['beach','jamaica','take+it+easy','slow+motion','time', 'swim+suit']
-            return tag = '&tag=' + arr[int];
+            arr= ['beach','jamaica','take+it+easy','slow+motion','time', 'bikini', '420']
+            return tag = arr[int]+apiKey;
         } else if(aud.currentTime > 895 && aud.currentTime < 1168){
             timerRule = false;
-            arr= ['bernie+sanders','porn','drugs','money','win', 'donald+trump']
-            return tag = '&tag=' + arr[int];
+            arr= ['bernie+sanders','porn','drugs','money','win', 'donald+trump', 'oil']
+            return tag = arr[int]+apiKey;
         } else if(aud.currentTime > 1168 && aud.currentTime < 1400){
             timerRule = true;
-            arr= ['secret','doin+it','want+it','i+dont+know','strippers', 'sex']
-            return tag = '&tag=' + arr[int];
+            arr= ['secret','doin+it','want+it','i+dont+know','strippers', 'sex', 'boobs']
+            return tag = arr[int]+apiKey;
         } else if(aud.currentTime > 1400 && aud.currentTime < 1725){
             timerRule = true;
-            arr= ['funk','dance+party','going+out','aww+yeah','ftw']
-            return tag = '&tag=' + arr[int];
+            arr= ['funk','dance+party','going+out','aww+yeah','ftw', 'dance+floor', 'get+down']
+            return tag = arr[int]+apiKey;
         } else if(aud.currentTime > 1725 && aud.currentTime < 1825){
-            arr= ['drums','conga','bass+player','the+beat','clap']
+            arr= ['drums','conga','bass+player','the+beat','clap', 'rhythm', 'bass']
             return tag = '&tag=' + arr[int];
         } else if(aud.currentTime > 1825 && aud.currentTime < 1900){
-            arr= ['the+count','ten','start+over','1-2-3','counting']
-            return tag = '&tag=' + arr[int];
+            arr= ['the+count','ten','start+over','1-2-3','counting', '', 'one+two+three']
+            return tag = arr[int]+apiKey+apiKey;
         }
         else {
             return '';
